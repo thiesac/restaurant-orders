596f8aebba1635582f839803755cc6129f1b965e
Thiesa <thiesa.c@gmail.com>
2023-11-08 13:49:50 -0400

    feat: Req3 csv reader

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
 # Req 3
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.set = [False]
+
+    def read_csv_rows(self):
+        with open(self.source_path, newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                yield row
+
+    def print_csv_rows(self):
+        for row in self.read_csv_rows():
+            print(row)
+
+
+menu_data = MenuData("tests/mocks/menu_base_data.csv")
+menu_data.print_csv_rows()
