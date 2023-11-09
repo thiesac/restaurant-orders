a3dbd7d43e16ab38b31a5c4e74f87d665aaa3953
Thiesa <thiesa.c@gmail.com>
2023-11-09 12:39:36 -0400

    feat: get_main_menu not complete

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
+# from models.ingredient import Restriction
 
 DATA_PATH = "data/menu_base_data.csv"
 INVENTORY_PATH = "data/inventory_base_data.csv"
@@ -26,4 +27,40 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        if restriction is None:
+            return [
+                {
+                    "dish_name": dish.name,
+                    "ingredients": [
+                        {"name": ingredient.name, "quantity": quantity}
+                        for ingredient, quantity in dish.recipe.items()
+                    ],
+                    "price": dish.price,
+                    "restrictions": [str(r) for r in dish.get_restrictions()],
+                }
+                for dish in self.menu_data.dishes
+            ]
+        else:
+            filtered_dishes = [
+                {
+                    "dish_name": dish.name,
+                    "ingredients": [
+                        {"name": ingredient.name, "quantity": quantity}
+                        for ingredient, quantity in dish.recipe.items()
+                    ],
+                    "price": dish.price,
+                    "restrictions": [str(r) for r in dish.get_restrictions()],
+                }
+                for dish in self.menu_data.dishes
+                if restriction not in dish.get_restrictions()
+            ]
+            return filtered_dishes
+
+
+# menu_builder = MenuBuilder()
+# # menu_builder.make_order("lasanha presunto")
+# print(menu_builder)
+# print(menu_builder.menu_data.dishes)
+# menu_builder.get_main_menu(restriction=Restriction.ANIMAL_MEAT)
+
+# # python3 -m src.services.menu_builder
