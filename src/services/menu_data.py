f57f9e6ae51f78166d238463a3c4cc11a4f75ba8
Thiesa <thiesa.c@gmail.com>
2023-11-09 09:27:20 -0400

    feat: Req3 menu_data complete

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,22 +1,61 @@
-# Req 3
 import csv
+from typing import Dict, Set
+
+from models.dish import Dish
+from models.ingredient import Ingredient
 
 
 class MenuData:
+    # Initialize the class with the source path for the CSV file
     def __init__(self, source_path: str) -> None:
+        # Set the source path to the provided path
         self.source_path = source_path
-        self.dishes = set()
+        # Initialize an empty set to store the dishes
+        self.dishes: Set[Dish] = set()
+        # Call the private method to read the CSV file
+        self._read_csv()
 
-    def read_csv_rows(self):
+    # Private method to read the CSV file
+    def _read_csv(self):
         with open(self.source_path, newline="") as csvfile:
-            reader = csv.reader(csvfile)
+            # Use csv.DictReader to read the rows as dictionaries
+            reader = csv.DictReader(csvfile)
+            # Create a dictionary to map dish names to Dish instances
+            dishes_map: Dict[str, Dish] = {}
+
             for row in reader:
-                yield row
+                dish_name, price = self._extract_dish_info(row, dishes_map)
+                ingredient, recipe_amount = self._extract_ingredient_info(row)
 
-    def print_csv_rows(self):
-        for row in self.read_csv_rows():
-            print(row)
+                if dish_name not in dishes_map:
+                    # If dish not in dishes_map, create a new Dish instance
+                    # and add it to dishes_map and self.dishes
+                    dish = Dish(dish_name, price)
+                    dishes_map[dish_name] = dish
+                    self.dishes.add(dish)
+                else:
+                    # If the dish is already in dishes_map, get
+                    # the corresponding Dish instance
+                    dish = dishes_map[dish_name]
+
+                # Create an Ingredient instance with the extracted
+                # ingredient name
+                ingredient = Ingredient(ingredient)
+                # Add the ingredient and recipe amount to the dish's recipe
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
+
+    # Private method to extract dish information from a row
+    def _extract_dish_info(self, row, dishes_map):
+        dish_name = row["dish"]
+        price = float(row["price"])
+        return dish_name, price
+
+    # Private method to extract ingredient information from a row
+    def _extract_ingredient_info(self, row):
+        ingredient_name = row["ingredient"]
+        recipe_amount = float(row["recipe_amount"])
+        return ingredient_name, recipe_amount
 
 
-menu_data = MenuData("tests/mocks/menu_base_data.csv")
-menu_data.print_csv_rows()
+# menu_data = MenuData("tests/mocks/menu_base_data.csv")
+# print(menu_data.dishes)
